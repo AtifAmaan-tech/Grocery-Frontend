74e0109a976f114bf264981c574fd400a7e4fbda
AtifMr <atifamaan628@gmail.com>
2026-01-30 02:04:16 +0530

    adds hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Grocery from "../../assets/grocery.png";
+import Button from "../Button/Button";
+const Hero = () => {
+  return (
+    <section>
+      <div className="max-w-[1400px] min-h-screen mx-auto px-10 flex md:flex-row flex-col items-center md:pt-25 pt-20">
+        {/* Hero Content */}
+        <div className="flex-1">
+          <span className="bg-green-100 text-green-500 px-3 py-1 text rounded-full mb-4">
+            Export Best Quality...
+          </span>
+          <h1 className="md:text-7xl/20 text-5xl/14 font-bold">
+            Tasty Organic <span className="text-orange-500">Fruites</span> & <span className="text-green-500">Veggies</span> in Your
+            City
+          </h1>
+          <p className="text-zinc-600 text-lg max-w-[530px] mt-5 mb-7">Bread for a high content of beneficial substance. Our products are all fresh and healthy. </p>
+          <Button content="Shop Now" />
+        </div>
+        {/* Hero Image */}
+        <div className="flex-1">
+          <img src={Grocery} alt="Hero Image" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import { FaBagShopping } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
+import { CgMenuMotion } from "react-icons/cg";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+
+  }
+  
+
+  return (
+    <header className="bg-white fixed top-0 left-0 right-0 py-4 md:py-0">
+      <nav className="max-w-[1400px] mx-auto md:h-[14vh] [12vh] px-10 flex justify-between items-center">
+        {/* Main logo */}
+        <a href="#" className="text-3xl semi-bold">
+          Fresh<span className="font-semibold text-green-500 uppercase">c</span>
+          art
+        </a>
+
+        {/* Desktop Menu */}
+        <ul className="md:flex items-centre gap-x-15 hidden ">
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-green-500"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-green-500"
+            >
+              About Us
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-green-500"
+            >
+              Process
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-green-500"
+            >
+              Contact Us
+            </a>
+          </li>
+        </ul>
+
+        {/* Nav Action */}
+        <div className="flex items-center gap-x-5">
+          {/* Input Field */}
+          <div className="md:flex p-1 border-2 border-green-500 rounded-full hidden">
+            <input
+              type="text"
+              id="text"
+              name="text"
+              placeholder="search items"
+              autoComplete="off"
+              className="flex-1 h-[4vh] px-3 focus:outline-none"
+            ></input>
+            <button className="bg-green-500 text-white w-8 h-8 flex justify-center items-center rounded-full bg-gradient-to-b from-green-400 to-green-500">
+              <FaSearch />
+            </button>
+          </div>
+          <a href="#" className="text-zinc-800 text-2xl">
+            <FaHeart />
+          </a>
+          <a href="#" className="text-zinc-800 text-2xl">
+            <FaBagShopping />
+          </a>
+
+          {/* Hamburger */}
+          <a href="#" className={"text-zinc-300 text-3xl md:hidden"} onClick={ () => setShowMenu(prev => !prev)}>
+           { showMenu? <CgMenuMotion /> : <IoMenu />}
+          </a>
+        </div>
+
+        {/* Mobile Menu */}
+        <ul className={`flex flex-col items-center text-center md:hidden gap-y-12 bg-green-500/20 backdrop-blur-xl absolute top-19 transition-all duration-300 ${showMenu ? '-right-1/3': '-right-full pl-5'} p-10 transform -translate-x-1/2 rounded-lg`}>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-green-500"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-green-500"
+            >
+              About Us
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-green-500"
+            >
+              Process
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="font-semibold tracking-wider text-zinc-800 hover:text-green-500"
+            >
+              Contact Us
+            </a>
+          </li>
+
+          <li className="flex p-1 border-2 border-green-500 rounded-full md:hidden">
+            <input
+              type="text"
+              id="text"
+              name="text"
+              placeholder="search items"
+              autoComplete="off"
+              className="flex-1 h-[4vh] px-3 focus:outline-none"
+            ></input>
+            <button className="bg-green-500 text-white w-8 h-8 flex justify-center items-center rounded-full bg-gradient-to-b from-green-400 to-green-500">
+              <FaSearch />
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
