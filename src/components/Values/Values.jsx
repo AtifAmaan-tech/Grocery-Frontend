db125fb9e654e9a84624c6c0c3561688ebceffbb
AtifMr <atifamaan628@gmail.com>
2026-03-16 16:53:19 +0530

    Completes Values section

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -7,14 +7,16 @@ const Values = () => {
   //Left Values
   const leftValues = values.slice(0, 2).map((item) => {
     return (
-      <div className="flex">
-        <div className = 'text-right'>
-          <h3 className="text-3xl font-bold">{item.title}</h3>
-          <p>{item.description}</p>
+      <div className="flex md:flex-row-reverse gap-6 items-center">
+        <div>
+          <span className="flex justify-center items-center text-3xl text-white bg-gradient-to-b from-green-400 to-green-500 w-15 h-15 rounded-full">
+            {item.icon}
+          </span>
         </div>
-        <span className="flex justify-center items-center text-3xl text-white bg-gradient-to-b from-green-400 to-green-500 w-15 h-15 rounded-full">
-          {item.icon}
-        </span>
+        <div className="md:text-right">
+          <h3 className="text-3xl font-bold">{item.title}</h3>
+          <p className= 'text-zinc-600 py-2'>{item.description}</p>
+        </div>
       </div>
     );
   });
@@ -22,14 +24,16 @@ const Values = () => {
   //Right Values
   const rightValues = values.slice(2).map((item) => {
     return (
-      <div className="flex flex-row-reverse">
-        <div className = ''>
+      <div className="flex flex-row-reverse gap-6 items-center">
+        <div className="">
           <h3 className="text-zinc-800 text-3xl font-bold">{item.title}</h3>
-          <p>{item.description}</p>
+          <p className= 'text-zinc-600 py-2'>{item.description}</p>
         </div>
-        <span className="flex justify-center items-center text-3xl text-white bg-gradient-to-b from-green-400 to-green-500 w-15 h-15 rounded-full">
-          {item.icon}
-        </span>
+        <div>
+          <span className="flex justify-center items-center text-3xl text-white bg-gradient-to-b from-green-400 to-green-500 w-15 h-15 rounded-full">
+            {item.icon}
+          </span>
+        </div>
       </div>
     );
   });
@@ -38,15 +42,15 @@ const Values = () => {
     <section>
       <div className="max-w-[1400px] mx-auto px-10 py-10">
         <Heading highlight="Our" content="Values" />
-        <div className="flex">
+        <div className="flex flex-col md:flex-row mt-13 md:gap-5 gap-6">
           {/* Left section */}
-          <div>{leftValues}</div>
+          <div className = 'md:min-h-100 flex flex-col md:justify-between gap-6'>{leftValues}</div>
           {/* Image section  */}
-          <div className="w-1/2">
-            <img src={basket}></img>
+          <div className="md:flex w-1/2 hidden">
+            <img src={basket} className='w-100 h-100 object-cover mx-auto'></img>
           </div>
           {/* Right Section */}
-          <div>{rightValues}</div>
+          <div className = 'md:min-h-100 flex flex-col md:justify-between gap-6' >{rightValues}</div>
         </div>
       </div>
       ;
